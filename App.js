a99d0a8954ba010dedcba08cd42b0cebe4cf2454
JesseVent <jessevent@icloud.com>
2017-09-10 00:38:23 +0930

    Added reset button
    Added reset button to clear cookies

---
+++ App.js
@@ -0,0 +1,199 @@
+import React, {
+  Component
+} from 'react';
+// import logo from './logo.svg';
+
+import Cookies from 'js-cookie';
+import CoinContainer from './component/CoinContainer';
+import Helper from './class/Helper';
+import './css/App.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // state {if the value is not used in render it should not be in state}
+    this.state = {
+      feed: null,
+      filteredCoins: typeof Cookies.get('filteredCoins') === 'undefined' ? '' : Cookies.get('filteredCoins')
+    };
+    this.totalAmount = 0;
+    // init load
+    this.loadFeed();
+
+    // bind this to functions
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmitCoinAmount = this.handleSubmitCoinAmount.bind(this);
+    this.handleExpand = this.handleExpand.bind(this);
+    this.resetCoins = this.resetCoins.bind(this);
+
+    this.helper = new Helper();
+  }
+  componentDidMount() {
+    // save so you can clear this interval later - load data every 15 seconds
+    this.streamId = setInterval(this.loadFeed.bind(this), 15000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.streamId);
+  }
+
+  /**
+   * get coinmarket feed data
+   * @return {[type]} [description]
+   */
+  loadFeed() {
+    console.log('getting feed data');
+    let xhttp = new XMLHttpRequest();
+    let self = this;
+    xhttp.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        self.setState({
+          feed: self.helper.filterCoins(JSON.parse(this.responseText))
+        });
+        self.filterSbmt.disabled = false;
+        document.getElementById('loading-gif').style.visibility = 'hidden';
+      }
+
+    };
+    xhttp.open("GET", "https://api.coinmarketcap.com/v1/ticker/?convert=aud", true);
+    xhttp.send()
+  }
+
+    resetCoins() {
+      console.log('Resetting Coins')
+      let filteredCoins = Cookies.remove('filteredCoins');
+      this.loadFeed();
+    }
+  /**
+   * save filter preference in cookie
+   * @param  {[type]} event [description]
+   * @return {[type]}       [description]
+   */
+  handleSubmit(event) {
+    event.preventDefault();
+    this.filterSbmt.disabled = true;
+    // save coins to filter in a cookie
+    Cookies.set('filteredCoins', this.state.filteredCoins, {
+      expires: 364
+    });
+    this.loadFeed();
+  }
+
+  /**
+   * save coin amount in cookie
+   * @param  {[type]} event [description]
+   * @return {[type]}       [description]
+   */
+  handleSubmitCoinAmount(event) {
+    event.preventDefault();
+    this.amountSbmt.disabled = true;
+    // save coin amount in local storage
+    localStorage.setItem(this.state.coinId.toUpperCase(), this.state.coinAmount);
+    this.setState({
+      coinId: "",
+      coinAmount: ""
+    });
+    this.amountSbmt.disabled = false;
+  }
+
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleExpand(){
+    this.expand.classList.toggle('expand');
+  }
+  renderSpinners() {
+    if (this.state.feed !== null) {
+      let feed = this.state.feed;
+      let getColor = this.helper.getColor;
+      let formatNumber = this.helper.formatNumber;
+      let convertTimestamp = this.helper.convertTimestamp;
+      let coins = [];
+      let coinTotal = 0;
+
+      let coinContainers = feed.map(function(feed, i) {
+          // change color of percentage numbers
+          let classes = {
+            hour: getColor(feed.percent_change_1h),
+            day: getColor(feed.percent_change_24h),
+            day7: getColor(feed.percent_change_7d)
+          }
+          let percent = {
+            hour: feed.percent_change_1h,
+            day: feed.percent_change_24h,
+            day7: feed.percent_change_7d
+          }
+
+          let holdAmount = typeof localStorage[feed.symbol] === 'undefined' ? 0 : localStorage[feed.symbol] * feed.price_aud;
+          let holdings = ( <p> Worth: {formatNumber(holdAmount)} </p>);
+          coinTotal += holdAmount;
+          let name = '';
+          // short names only
+          if(feed.name.length>9){
+            name = feed.symbol;
+          }else{
+            name = feed.name;
+          }
+          let lastUpdated = convertTimestamp(feed.last_updated);
+          return (
+                <CoinContainer key={i} name={name} price_usd={formatNumber(feed.price_aud)} last_updated = {lastUpdated} classes={classes} percent={percent}>
+                  {holdings}
+                </CoinContainer>
+          );
+      });
+      // but 3 coin containers in a row div
+      while (coinContainers.length > 0) {
+        let row = (<div key={Math.random()} className="row">{coinContainers.splice(0,3)}</div>);
+        coins.push(row);
+      }
+      // Only display the total coin value if "hold" values have been entered
+      if (coinTotal > 0) {
+        document.getElementById('coin-total').textContent = 'Total: ' + formatNumber(coinTotal);
+        document.title = formatNumber(coinTotal) + ' | Crypto';
+      }
+
+      return coins;
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="nine columns">
+            <div id="coin-total"></div>
+          </div>
+          <div className="three columns expand-btn-container">
+            <button className="button-primary" id="expand-btn" onClick={this.handleExpand}>Apply Filters</button>
+            <button className="button-primary" id="reset-btn" onClick={this.resetCoins}>Reset Filters</button>
+          </div>
+          <div className="twelve columns expand " ref={(value)=>this.expand = value}>
+            <form onSubmit={this.handleSubmit} >
+                <label for="filter-coins">What coins do you have?</label>
+                <input id="filter-coins" type="text" name="filteredCoins" value={this.state.filteredCoins} onChange={this.handleInputChange} placeholder="btc,eth,xrp" required/>
+                <input className="button-primary" type="submit" value="Apply Filter" ref={(value) => this.filterSbmt = value} />
+            </form>
+            <form onSubmit={this.handleSubmitCoinAmount} >
+                <label>How many coins do you have?</label>
+                <input type="text" name="coinId" value={this.state.coinId} onChange={this.handleInputChange} placeholder="Coin Symbol e.g btc,eth,ltc" required />
+                <input type="number" step="any" min="0" name="coinAmount" value={this.state.coinAmount} onChange={this.handleInputChange} placeholder="Quantity of coins i.e. 123.04" required />
+                <input className="button-primary" type="submit" value="Add" ref={(value) => this.amountSbmt = value} />
+            </form>
+          </div>
+        </div>
+        <img className="loading-gif" id="loading-gif" style={{display: 'block',margin: "0 auto",height: '100px',width: '500px'}} src="./loading.gif " alt="Loading..."></img>
+        {this.renderSpinners()}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Helper.js
@@ -0,0 +1,99 @@
+import Cookies from 'js-cookie';
+/**
+ * helper class
+ */
+export default class Helper {
+  /**
+   * get color based on a number > 0, green > red
+   * @param  {[type]} feed    [new feed]
+   * @param  {[type]} oldFeed
+   * @return {[type]}  string
+   */
+  getColor(num) {
+      if (num > 0) {
+        return "green";
+      } else {
+        return "red";
+      }
+  }
+
+  /**
+   * returned filtered coin data - if there is no prefrence return top 10 coins
+   * @param  {[type]} coins         [all coins]
+   * @param  {[type]} filteredCoins [coins to get]
+   * @return {[type]}               [description]
+   */
+  filterCoins(coins) {
+    let filteredCoins = Cookies.get('filteredCoins');
+    if (typeof filteredCoins === 'undefined' || filteredCoins.replace(' ', '') === "") {
+      return coins.filter(function(coin) {
+        for (var i = 0; i < 24; i++) {
+          if (coin.rank * 1 === i + 1) {
+            return 1;
+          }
+        }
+        return 0;
+      });
+    } else {
+      filteredCoins = filteredCoins.replace(' ', '').split(',');
+      return coins.filter(function(coin) {
+        for (var i = 0; i < filteredCoins.length; i++) {
+          if (filteredCoins[i].toUpperCase() === coin.symbol) {
+            return 1;
+          }
+        }
+        return 0;
+      });
+    }
+  }
+  /**
+   * Format numbers for better readability
+   * @param  {[type]} num
+   * @return {[type]} string
+   */
+  formatNumber(num) {
+    // Ensure value is a number first
+    num = num * 1;
+
+    // Source: https://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript#149099
+    var formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    });
+
+    return formatter.format(num);
+  }
+
+  /**
+  Format UNIX Datetimestamps into normal readable Datetimestamps
+  * @param {[type]} timestamp
+  * @return {[type]} time
+  */
+  convertTimestamp(timestamp) {
+  var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
+		yyyy = d.getFullYear(),
+		mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
+		dd = ('0' + d.getDate()).slice(-2),			// Add leading 0.
+		hh = d.getHours(),
+		h = hh,
+		min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
+		ampm = 'AM',
+		time;
+
+	if (hh > 12) {
+		h = hh - 12;
+		ampm = 'PM';
+	} else if (hh === 12) {
+		h = 12;
+		ampm = 'PM';
+	} else if (hh === 0) {
+		h = 12;
+	}
+
+	// ie: 2013-02-18, 8:35 AM
+	time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
+
+	return time;
+}
+}
